be08b5b848fedbb7658b52910e92aea23aa69fbe
Rupesh Garhwal <rupeshgarhwal3920@gmail.com>
2022-02-11 23:06:30 +0530

    test-route

--- api/index.js
+++ api/index.js
@@ -1,4 +1,11 @@
 const express = require("express");
 const router = express.Router();
 
+router.get("/", (req, res, next) => {
+	res.json({
+		status: "success",
+		message: "Hello from server",
+	});
+});
+
 module.exports = router;
