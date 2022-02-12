9e99f97fc70fb7dcb1124d50fdd0a06d84ac743e
Rupesh Garhwal <rupeshgarhwal3920@gmail.com>
2022-02-12 20:02:48 +0530

    routes - users, utils

---
+++ api/users/controllers/updateUserController.js
@@ -0,0 +1,5 @@
+const catchAsync = require("../../../utils/catchAsync");
+
+exports.updateProfile = catchAsync(async (req, res, next) => {});
+
+exports.addAadhar = catchAsync(async (req, res, next) => {});
