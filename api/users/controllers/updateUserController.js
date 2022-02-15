582780eec8140e98979569db2253efc580fc0c84
Rupesh Garhwal <rupeshgarhwal3920@gmail.com>
2022-02-16 01:02:30 +0530

    Users Operations API (#5)
    * controllers added
    
    * testing and changes

--- api/users/controllers/updateUserController.js
+++
@@ -1,5 +0,0 @@
-const catchAsync = require("../../../utils/catchAsync");
-
-exports.updateProfile = catchAsync(async (req, res, next) => {});
-
-exports.addAadhar = catchAsync(async (req, res, next) => {});
--- api/users/index.js
+++ api/users/index.js
@@ -1,17 +1,89 @@
 const express = require("express");
 const router = express.Router();
 
-const createUserController = require("./controllers/createUserController");
-const updateUserController = require("./controllers/updateUserController");
+const UserController = require("./controllers/userController");
+const auth = require("../auth/middlewares/authMiddlewares");
 
-// hq role
-router.post("/create/police", createUserController.createPoliceUser);
+// hq - admin role
+router.post(
+	"/create/police",
+	auth.protect,
+	auth.restrictTo("hq", "admin"),
+	UserController.createPoliceUser
+);
+router.delete(
+	"/delete/police/:id",
+	auth.protect,
+	auth.restrictTo("hq", "admin"),
+	UserController.deletePoliceUser
+);
+router.get(
+	"/police",
+	auth.protect,
+	auth.restrictTo("hq", "admin"),
+	UserController.getPoliceUsers
+);
+router.get(
+	"/police/:id",
+	auth.protect,
+	auth.restrictTo("hq", "admin"),
+	UserController.getPoliceUser
+);
+router.patch(
+	"/police/:id",
+	auth.protect,
+	auth.restrictTo("hq", "admin"),
+	UserController.updatePoliceUser
+);
+
+// HQ - Admin - Police
+router.get(
+	"/user",
+	auth.protect,
+	auth.restrictTo("hq", "admin", "police"),
+	UserController.getUsers
+);
+router.get(
+	"/user/:id",
+	auth.protect,
+	auth.restrictTo("hq", "admin", "police"),
+	UserController.getUser
+);
 
 // admin role
-router.post("/create/hq", createUserController.createHQUser);
+router.post(
+	"/create/hq",
+	auth.protect,
+	auth.restrictTo("admin"),
+	UserController.createHQUser
+);
+router.delete(
+	"/delete/hq/:id",
+	auth.protect,
+	auth.restrictTo("admin"),
+	UserController.deleteHQUser
+);
+router.get(
+	"/hq",
+	auth.protect,
+	auth.restrictTo("admin"),
+	UserController.getHQUsers
+);
+router.get(
+	"/hq/:id",
+	auth.protect,
+	auth.restrictTo("admin"),
+	UserController.getHQUser
+);
+router.patch(
+	"/hq/:id",
+	auth.protect,
+	auth.restrictTo("admin"),
+	UserController.updateHQUser
+);
 
 // all
-router.patch("/update/me", updateUserController.updateProfile);
-router.patch("/update/me/aadahar", updateUserController.addAadhar);
+router.patch("/update/me", auth.protect, UserController.updateMe);
+router.get("/me", auth.protect, UserController.getMe);
 
 module.exports = router;
