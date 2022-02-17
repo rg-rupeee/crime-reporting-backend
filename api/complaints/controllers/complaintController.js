903f83d374b0cafda74d20263bf3d809f672636f
Rupesh Garhwal <rupeshgarhwal3920@gmail.com>
2022-02-17 23:13:03 +0530

    initial commit

--- api/complaints/controllers/complaintController.js
+++ api/complaints/controllers/complaintController.js
@@ -1,3 +1,11 @@
 const catchAsync = require("../../../utils/catchAsync");
+const factory = require("../../../controllers/handlerFactory");
+const Complaint = require("../../../models/Complaints");
 
 exports.getNearbyComplaints = catchAsync(async (req, res, next) => {});
+
+exports.getAllComplaints = factory.getAll(Complaint);
+exports.getComplaint = factory.getOne(Complaint);
+exports.createComplaint = factory.createOne(Complaint);
+exports.updateComplaint = factory.updateOne(Complaint);
+exports.deleteComplaint = factory.deleteOne(Complaint);
--- api/complaints/controllers/generalUserComplaintController.js
+++ api/complaints/controllers/generalUserComplaintController.js
@@ -1,7 +1,12 @@
 const catchAsync = require("../../../utils/catchAsync");
+const factory = require("../../../controllers/handlerFactory");
+const Complaint = require("../../../models/Complaints");
 
-exports.createComplaint = catchAsync(async (req, res, next) => {});
-
-exports.getMyComplaints = catchAsync(async (req, res, next) => {});
+exports.getMyComplaints = catchAsync(async (req, res, next) => {
+	req.query = {
+		user: req.user.id,
+	};
+	return next();
+});
 
 exports.getMyComplaint = catchAsync(async (req, res, next) => {});
