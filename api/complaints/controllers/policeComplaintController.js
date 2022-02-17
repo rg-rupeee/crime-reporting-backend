676e3ab57e1236786675175aa350aacf8e438413
Rupesh Garhwal <rupeshgarhwal3920@gmail.com>
2022-02-17 23:13:40 +0530

    complaints (#7)

--- api/complaints/controllers/policeComplaintController.js
+++ api/complaints/controllers/policeComplaintController.js
@@ -1,6 +1,11 @@
 const catchAsync = require("../../../utils/catchAsync");
 
-exports.getAssignedComplaints = catchAsync(async (req, res, next) => {});
+exports.getAssignedComplaints = catchAsync(async (req, res, next) => {
+	req.query = {
+		AssignedOfficer: req.user.id,
+	};
+	return next();
+});
 
 exports.getAssignedComplaint = catchAsync(async (req, res, next) => {});
 
--- api/complaints/index.js
+++ api/complaints/index.js
@@ -3,19 +3,30 @@ const router = express.Router();
 
 const generalUserComplaintController = require("./controllers/generalUserComplaintController");
 const policeComplaintController = require("./controllers/policeComplaintController");
-const HQandAdminComplaintController = require("./controllers/HQandAdminComplaintController");
 const complaintController = require("./controllers/complaintController");
+const auth = require("../auth/middlewares/authMiddlewares");
 
 // all role
 router.get("/nearby/:lat/:long", complaintController.getNearbyComplaints);
 
 // user role
-router.post("/", generalUserComplaintController.createComplaint); //filters, sort
-router.get("/me", generalUserComplaintController.getMyComplaints);
+router.post("/", complaintController.createComplaint);
+router.get(
+	"/me",
+	auth.protect,
+	auth.restrictTo("user"),
+	generalUserComplaintController.getMyComplaint,
+	complaintController.getAllComplaints
+);
 router.get("/me/:id", generalUserComplaintController.getMyComplaint);
 
 // police role
-router.get("/me/assigned", policeComplaintController.getAssignedComplaints); //filters, sort
+router.get(
+	"/me/assigned",
+	auth.protect,
+	auth.restrictTo("police"),
+	complaintController.getAllComplaints
+);
 router.get("/me/assigned/:id", policeComplaintController.getAssignedComplaint);
 router.patch(
 	"/me/assigned/:id",
@@ -23,8 +34,23 @@ router.patch(
 );
 
 // hq and admin role
-router.get("/", HQandAdminComplaintController.getAllComplaints); // filters, sort
-router.get("/:id", HQandAdminComplaintController.getComplaint);
-router.patch("/:id", HQandAdminComplaintController.updateComplaint);
+router.get(
+	"/",
+	auth.protect,
+	auth.restrictTo("hq", "admin"),
+	complaintController.getAllComplaints
+);
+router.get(
+	"/:id",
+	auth.protect,
+	auth.restrictTo("hq", "admin"),
+	complaintController.getComplaint
+);
+router.patch(
+	"/:id",
+	auth.protect,
+	auth.restrictTo("hq", "admin"),
+	complaintController.updateComplaint
+);
 
 module.exports = router;
