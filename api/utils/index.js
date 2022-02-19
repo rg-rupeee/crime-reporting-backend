981e1671f2b1e37a8fe681a6632f94eb0d309bc5
Rupesh Garhwal <rupeshgarhwal3920@gmail.com>
2022-02-19 23:14:25 +0530

    upload tested

--- api/utils/index.js
+++ api/utils/index.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const fileUpload = require("express-fileupload");
 
 const uploadController = require("./controllers/uploadController");
+const auth = require("../auth/middlewares/authMiddlewares");
 
 router.use(
 	fileUpload({
@@ -11,8 +12,10 @@ router.use(
 );
 
 // all roles
-router.route("/upload/image").post(uploadController.uploadImage);
+router.route("/upload/image").post(auth.protect, uploadController.uploadImage);
 
-router.route("/upload/images").post(uploadController.uploadImages);
+router
+	.route("/upload/images")
+	.post(auth.protect, uploadController.uploadImages);
 
 module.exports = router;
