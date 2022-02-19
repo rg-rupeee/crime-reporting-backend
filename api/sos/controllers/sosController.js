07b44498a5ee39d3548bb90e1cacaf4f082b464c
Rupesh Garhwal <rupeshgarhwal3920@gmail.com>
2022-02-19 18:16:56 +0530

    sos removed

--- api/sos/controllers/sosController.js
+++
@@ -1,7 +0,0 @@
-const catchAsync = require("../../../utils/catchAsync");
-
-exports.getSavedContacts = catchAsync(async (req, res, next) => {});
-
-exports.addContacts = catchAsync(async (req, res, next) => {});
-
-exports.deleteContacts = catchAsync(async (req, res, next) => {});
