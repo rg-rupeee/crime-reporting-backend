34be20a7f17791e94e21ebd9173c0d5f5e8e39d3
Rupesh Garhwal <rupeshgarhwal3920@gmail.com>
2022-03-29 09:28:48 +0530

    send message

--- api/sos/controllers/messageController.js
+++ api/sos/controllers/messageController.js
@@ -1,8 +1,23 @@
+const twilio = require("twilio");
+
 const catchAsync = require("../../../utils/catchAsync");
 
+const client = twilio(process.env.ACCOUNT_SID, process.env.AUTH_TOKEN);
+
 exports.sendMessage = catchAsync(async (req, res, next) => {
-	return res.json({
-		status: "success",
-		message: "message sent succesfully",
-	});
+	const { message, phone } = req.body.message;
+
+	client.messages
+		.create({ body: message, from: "+918223088814", to: phone })
+		.then((message) => {
+			return res.json({
+				status: "success",
+				message: "message sent successfully",
+				data: message,
+			});
+		})
+		.catch((err) => {
+			console.log(err);
+			return res.status(400).json({ status: "fail", error: err });
+		});
 });
--- api/sos/index.js
+++ api/sos/index.js
@@ -2,8 +2,16 @@ const express = require("express");
 const router = express.Router();
 
 const messageController = require("./controllers/messageController");
+const auth = require("../auth/middlewares/authMiddlewares");
+const check = require("../../controllers/middlewares/check");
 
 // general and police
-router.post("/", messageController.sendMessage);
+router.post(
+	"/",
+	auth.protect,
+	auth.restrictTo("user", "police"),
+	check.requiredFields("message"),
+	messageController.sendMessage
+);
 
 module.exports = router;
